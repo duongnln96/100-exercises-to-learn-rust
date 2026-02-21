cdff0e2b8d5de9ed080a33e1ea84c095fa90825c
Duong Nguyen <nguyenlengocduong@gmail.com>
2026-02-21 20:16:10 +0700

    ticket v1

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,71 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+pub struct Order {
+    product_name: String,
+    quantity: usize,
+    unit_price: usize,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: usize, unit_price: usize) -> Order {
+        if product_name.is_empty() {
+            panic!("product name can't be empty!")
+        }
+        if product_name.len() > 300 {
+            panic!("product name cannot be longer than 300 bytes!")
+        }
+        if quantity <= 0 {
+            panic!("quantity must be strictly greater than zero!")
+        }
+        if unit_price <= 0 {
+            panic!("unit price must be strictly greater than zero!")
+        }
+
+        Order {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        if product_name.is_empty() {
+            panic!("product name can't be empty!")
+        }
+        if product_name.len() > 300 {
+            panic!("product name cannot be longer than 300 bytes!")
+        }
+        self.product_name = product_name
+    }
+
+    pub fn quantity(&self) -> &usize {
+        &self.quantity
+    }
+
+    pub fn set_quantity(&mut self, quantity: usize) {
+        if quantity <= 0 {
+            panic!("quantity must be strictly greater than zero!")
+        }
+        self.quantity = quantity
+    }
+
+    pub fn unit_price(&self) -> &usize {
+        &self.unit_price
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: usize) {
+        if unit_price <= 0 {
+            panic!("unit price must be strictly greater than zero!")
+        }
+        self.unit_price = unit_price
+    }
+
+    pub fn total(&self) -> usize {
+        self.quantity * self.unit_price
+    }
+}
